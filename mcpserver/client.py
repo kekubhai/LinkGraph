0e8f7e65037421535505dd3fcaefed258130f288
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-06-30 12:54:00 +0530

    Add langgraph dependency and implement client for multi-server MCP communication

---
+++ mcpserver/client.py
@@ -0,0 +1,34 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq  # <--- import this if using ChatGroq
+from langchain_core.messages import HumanMessage
+
+import asyncio
+import os
+
+load_dotenv()
+
+async def main():
+    client = MultiServerMCPClient({
+        "math": {
+            "command": "python",
+            "args": ["mathserver.py"],
+            "transport": "stdio",
+        },
+        "weather": {
+            "url": "http://localhost:8000/mcp/weather",
+            "transport": "streamable_http",
+        }
+    })
+
+    os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+    tools = await client.get_tools()
+    model = ChatGroq(model="qwen-qwq-32b")
+    agent = create_react_agent(model, tools)
+
+    math_response = await agent.ainvoke({"messages": [HumanMessage(content="What is 10+20?")]})
+    print(math_response)
+
+# run outside
+asyncio.run(main())
--- mcpserver/weather.py
+++ mcpserver/weather.py
@@ -1,3 +1,10 @@
- from mcp.server.fastmcp import FastMCP
- mcp=FastMCP("weather")
- @mcp.tool()
+from mcp.server.fastmcp import FastMCP
+mcp=FastMCP("weather")
+@mcp.tool()
+async def get_weather(city:str)->str:
+     """"Get the weather of the location"""
+     return f"The weather is always reaining in {city}"
+ 
+if __name__=="__main__":
+     mcp.run(transport="streamable_http")
+ 
